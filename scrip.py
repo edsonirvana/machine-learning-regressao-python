72e663b90ff5be235463c23fbbfe1859155ad952
edsonirvana <edsonirvana@gmail.com>
2020-12-05 20:45:55 -0300

    Update scrip.py

--- scrip.py
+++ scrip.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import statsmodels.formula.api as smf
 
+# Carregando o dataset
+df = pd.read_csv('dados/pesos.csv')
+
+# Criando o Modelo de Regressão
+estimativa = smf.ols(formula = 'Peso ~ Idade', data = df)
+
+# Treinando o Modelo de Regressão
+modelo = estimativa.fit()
+
+# Imprimindo o resumo do modelo
+print(modelo.summary())
